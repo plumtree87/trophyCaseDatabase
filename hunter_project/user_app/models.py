901497b0fd147e9a1d44cff00edfd250ef77c450
plumtree87 <plumtree87@protonmail.com>
2021-06-01 19:54:10 +0500

    created jwt authentication for users, it's effecting all my models now. Trying to figure out how to tell axios, 'hey this model doesn't need authenticated.'

--- hunter_project/user_app/models.py
+++ hunter_project/user_app/models.py
@@ -57,6 +57,12 @@ class User(AbstractBaseUser):
     # objects of this type.
     objects = UserManager()
 
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+
+    def has_module_perms(self, app_label):
+        return self.is_superuser
+
     def __str__(self):
         return self.email
 
